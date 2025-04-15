13a26920236e0699b1e46c850f970878b08e84ae
Kyumin Lee <79064939+rnaksdl@users.noreply.github.com>
2025-04-15 14:01:24 -0500

    fixed importing libcamera

--- record.py
+++ record.py
@@ -3,6 +3,7 @@
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import FileOutput
+from libcamera import Transform  # NEW IMPORT
 import time
 import os
 from datetime import datetime
